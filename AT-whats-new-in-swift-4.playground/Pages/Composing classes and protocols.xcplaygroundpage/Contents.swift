597ba33003d85099a38492884a4d9e9f9e84a112
Linh Vo D <linh.vo@asiantech.vn>
2017-08-06 15:38:05 +0700

    Composing classes and protocols

--- AT-whats-new-in-swift-4.playground/Pages/Composing classes and protocols.xcplaygroundpage/Contents.swift
+++ AT-whats-new-in-swift-4.playground/Pages/Composing classes and protocols.xcplaygroundpage/Contents.swift
@@ -1,7 +1,36 @@
 //: [Previous](@previous)
+/*:
+## Composing classes and protocols
 
+[SE-0156][SE-0156]: You can now write the equivalent of the Objective-C code `UIViewController <SomeProtocol> *` in Swift, i.e. declare a variable of a concrete type and constrain it to one or more protocols at the same time. The syntax is `let variable: SomeClass & SomeProtocol1 & SomeProtocol2`.
+
+[SE-0156]: https://github.com/apple/swift-evolution/blob/master/proposals/0156-subclass-existentials.md "Swift Evolution Proposal SE-0156: Class and Subtype existentials"
+*/
 import Foundation
+import UIKit
 
-var str = "Hello, playground"
+protocol HeaderView {}
+
+class ViewController: UIViewController {
+    let header: UIView & HeaderView
+
+    init(header: UIView & HeaderView) {
+        self.header = header
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init(coder decoder: NSCoder) {
+        fatalError("not implemented")
+    }
+}
+
+// Can't pass in a simple UIView that doesn't conform to the protocol
+// ViewController(header: UIView())
+// error: argument type 'UIView' does not conform to expected type 'UIView & HeaderView'
+
+// Must pass in an UIView (subclass) that also conforms to the protocol
+extension UIImageView: HeaderView {}
+
+ViewController(header: UIImageView()) // works
 
 //: [Next](@next)
